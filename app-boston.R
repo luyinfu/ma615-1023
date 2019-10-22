e9e22bc00bb5ebaf2a1a58cedba5a36087379053
luyinfu <luyinfu@bu.edu>
2019-10-22 18:58:29 -0400

    commit

--- app-boston.R
+++ app-boston.R
@@ -45,7 +45,8 @@ ui <- fluidPage(
         ),
 
         mainPanel(
-            leafletOutput(outputId = "mymap")
+            leafletOutput(outputId = "mymap"),
+            width = 12
         )
     )
 )
